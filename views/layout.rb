d1a162ec6a502f21d85a88c050998d195e68b789
vajrapani666 <thomas.devol@gmail.com>
2012-09-03 22:07:36 -0700

    Copy changes, CSS for fixed footer

--- views/layout.rb
+++ views/layout.rb
@@ -9,20 +9,22 @@ module Necrohost
         end
 
         def render template, options = {}
-          if options[:yield]
-            # this is a patch to allow us to use haml with mustache :{/
-            # we want the level of indent when the yield was referenced
-            # but account for the yield having its own indent
-            # that number 2 is a magic constant at the moment
-            indentation_level = template.to_s[/(\s+)v = ctx/,1].length - HAML_INDENT_SIZE
-            unindented_tail = options[:yield].lines.to_a[1..-1]
-            indented_tail = unindented_tail.collect do |line|
-              "#{" " * indentation_level}#{line}"
-            end.join
-            indented_yield = options[:yield].lines.first + indented_tail
-            options[:yield] = indented_yield
-          end
+          options[:yield] = indented_yield(template, options[:yield])
           super template, options
+        end
+
+        def indented_yield haml_template, to_yield
+          return to_yield unless to_yield
+          # this is a patch to allow us to use haml with mustache :{/
+          # we want the level of indent when the yield was referenced
+          # but account for the yield having its own indent
+          # that number 2 is a magic constant at the moment
+          indentation_level = haml_template.to_s[/(\s+)v = ctx/,1].length - HAML_INDENT_SIZE
+          unindented_tail = to_yield.lines.to_a[1..-1]
+          indented_tail = unindented_tail.collect do |line|
+            "#{" " * indentation_level}#{line}"
+          end.join
+          to_yield.lines.first + indented_tail
         end
       end
     end
